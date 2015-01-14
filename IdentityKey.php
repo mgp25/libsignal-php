0d16ffb7d348816065294f63c9d316f4e21fcba7
mgp25 <me@mgp25.com>
2015-01-14 23:59:16 +0100

    Update (revision required)

---
+++ IdentityKey.php
@@ -0,0 +1,52 @@
+<?php
+require_once("ecc/Curve.php");
+require_once("ecc/ECPublicKey.php");
+require_once("util/Hex.php");
+class IdentityKey {
+	protected $publicKey;	// ECPublicKey
+	private function __init() { // default class members
+	}
+	public static function __staticinit() { // static class members
+	}
+	public static function constructor__de8cd838 ($publicKey) // [ECPublicKey publicKey]
+	{
+		$me = new self();
+		$me->__init();
+		$me->publicKey = $publicKey;
+		return $me;
+	}
+	public static function constructor__29e7cc9a ($bytes, $offset) // [byte[] bytes, int offset]
+	{
+		$me = new self();
+		$me->__init();
+		$me->publicKey = Curve::decodePoint($bytes, $offset);
+		return $me;
+	}
+	public function getPublicKey ()
+	{
+		return $this->publicKey;
+	}
+	public function serialize ()
+	{
+		return $this->publicKey->serialize();
+	}
+	public function getFingerprint ()
+	{
+			/* match: ae1a4a6a */
+		return Hex::toString_ae1a4a6a($this->publicKey->serialize());
+	}
+	public function equals ($other) // [Object other]
+	{
+		if (($other == null))
+			return  FALSE ;
+		if (!($other instanceof IdentityKey))
+			return  FALSE ;
+		return $this->publicKey->equals(($other)::getPublicKey());
+	}
+	public function hashCode ()
+	{
+		return $this->publicKey->hashCode();
+	}
+}
+IdentityKey::__staticinit(); // initialize static vars for this class on load
+?>
---
+++ NoSessionException.php
@@ -0,0 +1,25 @@
+<?php
+class NoSessionException extends Exception {
+	private function __init() { // default class members
+	}
+	public static function __staticinit() { // static class members
+	}
+	public static function constructor__943a4c31 ($s) // [String s]
+	{
+		$me = new self();
+		$me->__init();
+		/* constructor resolution using types matched overloadcode: */
+		parent::constructor__($s);
+		return $me;
+	}
+	public static function constructor__cfc1f02f ($nested) // [Exception nested]
+	{
+		$me = new self();
+		$me->__init();
+		/* constructor resolution using types matched overloadcode: */
+		parent::constructor__($nested);
+		return $me;
+	}
+}
+NoSessionException::__staticinit(); // initialize static vars for this class on load
+?>
