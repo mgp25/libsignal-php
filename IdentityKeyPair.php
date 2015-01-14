2c20d744755459d001b85ba39b62d5ff38dd918e
archergod <sumit@sumitgupta.name>
2015-01-14 22:28:14 +0530

    finish the IdentityKeyPair

--- IdentityKeyPair.php
+++ IdentityKeyPair.php
@@ -13,6 +13,10 @@ class IdentityKeyPair {
 	 * @param IdentityKey publicKey 
 	 */
 	private $publicKey;
+	/**
+	 * 
+	 * @var ECPrivateKey
+	 */
 	private $privateKey;
 
 	public function __construct($key, $pKey="") {
@@ -21,38 +25,38 @@ class IdentityKeyPair {
 			$this->privateKey = $pKey;
 		}else if (is_array($key)){
 			try {
-				IdentityKeyPairStructure structure = IdentityKeyPairStructure.parseFrom(serialized);
-				$this->publicKey  = new IdentityKey(structure.getPublicKey().toByteArray(), 0);
-				$this->privateKey = Curve.decodePrivatePoint(structure.getPrivateKey().toByteArray());
+				$structure = IdentityKeyPairStructure::parseFrom($key);
+				$this->publicKey  = new IdentityKey($structure->getPublicKey()->toByteArray(), 0);
+				$this->privateKey = Curve.decodePrivatePoint($structure->getPrivateKey()->toByteArray());
 			} catch (Exception $e) {
 				throw new Exception($e);
 			}
 		}
 	}
 
-	public IdentityKeyPair(byte[] serialized) throws InvalidKeyException {
-		try {
-			IdentityKeyPairStructure structure = IdentityKeyPairStructure.parseFrom(serialized);
-			$this->publicKey  = new IdentityKey(structure.getPublicKey().toByteArray(), 0);
-			$this->privateKey = Curve.decodePrivatePoint(structure.getPrivateKey().toByteArray());
-		} catch (InvalidProtocolBufferException e) {
-			throw new InvalidKeyException(e);
-		}
+	/**
+	 * 
+	 * @return \libaxolotl\IdentityKey
+	 */
+	public function getPublicKey() {
+		return $this->publicKey;
 	}
 
-	public IdentityKey getPublicKey() {
-		return publicKey;
+	/**
+	 * 
+	 * @return \libaxolotl\ECPrivateKey
+	 */
+	public function getPrivateKey() {
+		return $this->privateKey;
 	}
 
-	public ECPrivateKey getPrivateKey() {
-		return privateKey;
-	}
-
-	public byte[] serialize() {
-		return IdentityKeyPairStructure.newBuilder()
-		.setPublicKey(ByteString.copyFrom(publicKey.serialize()))
-		.setPrivateKey(ByteString.copyFrom(privateKey.serialize()))
-		.build().toByteArray();
+	/**
+	 * Returns byte array
+	 */
+	public function serialize() {
+		return IdentityKeyPairStructure::newBuilder()->setPublicKey(ByteString.copyFrom($this->publicKey->serialize()))
+		->setPrivateKey(ByteString::copyFrom($this->privateKey->serialize()))
+		->build()->toByteArray();
 	}
 }
 ?>
