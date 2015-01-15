15bf6e8e7392b7fa64ba2e8f99c15f86184c9942
archergod <sumit@sumitgupta.name>
2015-01-15 21:23:24 +0530

    error correction and Constructor improvement test.

--- ecc/Curve.php
+++ ecc/Curve.php
@@ -30,7 +30,7 @@ class Curve {
 				foreach (range(0, (count($keyBytes) /*from: keyBytes.length*/ + 0)) as $_upto) $keyBytes[$_upto] = $bytes[$_upto - (0) + ($offset + 1)]; /* from: System.arraycopy(bytes, offset + 1, keyBytes, 0, keyBytes.length) */;
 				return DjbECPublicKey::constructor__ae1a4a6a($keyBytes);
 			default:
-				throw InvalidKeyException::constructor__943a4c31(("Bad key type: " . $type));
+				throw new InvalidKeyException(("Bad key type: " . $type));
 		}
 	}
 	public static function decodePrivatePoint ($bytes) // [byte[] bytes]
@@ -41,37 +41,37 @@ class Curve {
 	{
 		if (($publicKey->getType() != $privateKey->getType()))
 		{
-			throw InvalidKeyException::constructor__943a4c31("Public and private keys must be of the same type!");
+			throw new InvalidKeyException("Public and private keys must be of the same type!");
 		}
 		if (($publicKey->getType() == self::$DJB_TYPE))
 		{
-			return $Curve25519->calculateAgreement(($publicKey)::getPublicKey(), ($privateKey)::getPrivateKey());
+			return $Curve25519->calculateAgreement(call_user_func (array($publicKey,getPublicKey())) , call_user_func (array($privateKey,getPrivateKey())) );
 		}
 		else
 		{
-			throw InvalidKeyException::constructor__943a4c31(("Unknown type: " . $publicKey->getType()));
+			throw new InvalidKeyException(("Unknown type: " . $publicKey->getType()));
 		}
 	}
 	public static function verifySignature ($signingKey, $message, $signature) // [ECPublicKey signingKey, byte[] message, byte[] signature]
 	{
 		if (($signingKey->getType() == self::$DJB_TYPE))
 		{
-			return $Curve25519->verifySignature(($signingKey)::getPublicKey(), $message, $signature);
+			return $Curve25519->verifySignature(call_user_func (array($signingKey,getPublicKey())), $message, $signature);
 		}
 		else
 		{
-			throw InvalidKeyException::constructor__943a4c31(("Unknown type: " . $signingKey->getType()));
+			throw new InvalidKeyException(("Unknown type: " . $signingKey->getType()));
 		}
 	}
 	public static function calculateSignature ($signingKey, $message) // [ECPrivateKey signingKey, byte[] message]
 	{
 		if (($signingKey->getType() == self::$DJB_TYPE))
 		{
-			return $Curve25519->calculateSignature(self::getSecureRandom(), ($signingKey)::getPrivateKey(), $message);
+			return $Curve25519->calculateSignature(self::getSecureRandom(),call_user_func (array($signingKey,getPrivateKey())) , $message);
 		}
 		else
 		{
-			throw InvalidKeyException::constructor__943a4c31(("Unknown type: " . $signingKey->getType()));
+			throw new InvalidKeyException(("Unknown type: " . $signingKey->getType()));
 		}
 	}
 	protected static function getSecureRandom ()
