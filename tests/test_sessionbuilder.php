b1a25a57ab158a12b5c2a51ef3d171bfece7c8f3
mgp25 <me@mgp25.com>
2015-10-06 20:58:36 +0200

    remove debug

--- tests/test_sessionbuilder.php
+++ tests/test_sessionbuilder.php
@@ -24,70 +24,11 @@ function parseText($txt){
     }
     return $txt;
 }
-function niceVarDump($obj,$ident = 0){
-    $data = "";
-    $data .= str_repeat(" ",$ident);
-    $original_ident = $ident;
-    $toClose = false;
-    switch(gettype($obj)){
-        case "object":
-            $vars = (array) $obj;
-            $data .= gettype($obj)." (".get_class($obj).") (".count($vars).") {\n";
-            $ident += 2;
-            foreach($vars as $key=>$var){
-                $type = "";
-                $k = bin2hex($key);
-                if(strpos($k,"002a00") === 0){
-                    $k = str_replace("002a00", "", $k);
-                    $type = ":protected";
-                }
-                else if(strpos($k,bin2hex("\x00".get_class($obj)."\x00")) === 0){
-                    $k = str_replace(bin2hex("\x00".get_class($obj)."\x00"),"", $k);
-                    $type = ":private";
-                }
-                $k = hex2bin($k);
-                if(is_subclass_of($obj, "ProtobufMessage") && $k == "values"){
-                    $r = new ReflectionClass($obj);
-                    $constants = $r->getConstants();
-                    $newVar = [];
-                    foreach($constants as $ckey=>$cval){
-                        if(substr($ckey,0,3) != "PB_")
-                            $newVar[$ckey] = $var[$cval];
-                    }
-                    $var = $newVar;
-                }
-                $data .= str_repeat(" ", $ident)."[$k$type]=>\n".niceVarDump($var,$ident)."\n";
-            }
-            $toClose = true;
-        break;
-        case "array":
-            $data .= "array (".count($obj).") {\n";
-            $ident += 2;
-            foreach($obj as $key=>$val){
-                $data .= str_repeat(" ", $ident)."[".(is_integer($key)?$key:"\"$key\"")."]=>\n".niceVarDump($val,$ident)."\n";
-            }
-            $toClose = true;
-        break;
-        case "string":
-            $data .= "string \"".parseText($obj)."\"\n";
-        break;
-        case "NULL":
-            $data .= gettype($obj);
-        break;
-        default:
-            $data .= gettype($obj)."(".strval($obj).")\n";
-        break;
-    }
-    if($toClose)
-        $data .= str_repeat(" ", $original_ident)."}\n";
-
-    return $data;
-}
 class SessionBuilderTest extends PHPUnit_Framework_TestCase{
     const ALICE_RECIPIENT_ID = 5;
     const BOB_RECIPIENT_ID   = 2;
 
-    /*public function testBasicPreKeyV2(){
+    public function testBasicPreKeyV2(){
         $aliceStore = new InMemoryAxolotlStore();
         $aliceSessionBuilder = new SessionBuilder($aliceStore, $aliceStore, $aliceStore, $aliceStore, self::BOB_RECIPIENT_ID, 1);
 
@@ -168,7 +109,7 @@ class SessionBuilderTest extends PHPUnit_Framework_TestCase{
 
         return;
 
-    }*/
+    }
     /*
     public function test_basicPreKeyV3(){
         aliceStore = InMemoryAxolotlStore()
