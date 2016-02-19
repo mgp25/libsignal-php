d0d60bfbf0c547a25ac5887a14d57556f7a3ce7a
mgp25 <me@mgp25.com>
2016-02-19 01:18:02 +0100

    General update

--- src/util/guava/Supplier.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface Supplier {
-    private function __init() { // default class members
-    }
-    public static function __staticinit() { // static class members
-    }
-    abstract function get ();
-}
-Supplier::__staticinit(); // initialize static vars for this class on load
